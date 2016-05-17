e6c0c8a6456ea182afdb858c090e0ea31a65757f
PauliNiva <Pauli@Vulcan.local>
2016-05-17 14:17:11 +0300

    Spring works in ideaIDE

--- src/main/java/sotechat/Application.java
+++ src/main/java/sotechat/Application.java
@@ -2,12 +2,8 @@ package sotechat;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
