cd51e809655dd1f5445dab0219a9ab3bdaf31557
PauliNiva <Pauli@1BQQGNC.kumpula.dhcp.hupnet.helsinki.fi>
2016-05-23 14:06:27 +0300

    Few more test

---
+++ src/test/java/sotechat/controller/ChatControllerTest.java
@@ -0,0 +1,34 @@
+package sotechat.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class ChatControllerTest {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new ChatController()).build();
+    }
+
+    @Test
+    public void contextLoads() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/join").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
