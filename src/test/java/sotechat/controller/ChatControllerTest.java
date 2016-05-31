d6b617814fff88c56ef8b33acc35822a9a185f25
PauliNiva <Pauli@Vulcan.local>
2016-05-31 14:37:47 +0300

    tidying up controller test

--- src/test/java/sotechat/controller/ChatControllerTest.java
+++ src/test/java/sotechat/controller/ChatControllerTest.java
@@ -3,26 +3,15 @@ package sotechat.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import sotechat.data.Mapper;
 import sotechat.data.MapperImpl;
-
-import javax.servlet.http.HttpServletRequest;
-
-import java.security.Principal;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,9 +22,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringApplicationConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
 public class ChatControllerTest {
-
-    @Autowired
-    private WebApplicationContext context;
 
     /**
      * MockMvc.
