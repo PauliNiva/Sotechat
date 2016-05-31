9ae4c4ad32bca09fbde5da3cbf075accb9de7f20
PauliNiva <Pauli@Vulcan.local>
2016-05-31 14:25:55 +0300

    Added some unit tests

--- src/test/java/sotechat/controller/ChatControllerTest.java
+++ src/test/java/sotechat/controller/ChatControllerTest.java
@@ -3,16 +3,26 @@ package sotechat.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import sotechat.data.Mapper;
 import sotechat.data.MapperImpl;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.security.Principal;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,10 +34,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class ChatControllerTest {
 
+    @Autowired
+    private WebApplicationContext context;
+
     /**
      * MockMvc.
      */
     private MockMvc mvc;
+
     /**
      * Before.
      * @throws Exception
@@ -39,13 +53,23 @@ public class ChatControllerTest {
     }
 
     /**
-     * contextLoads.
+     * Get pyyntö polkuun "/join" palauttaa statukseksen OK.
      * @throws Exception
      */
     @Test
-    public void contextLoads() throws Exception {
+    public void getToJoinReturnOK() throws Exception {
         mvc.perform(MockMvcRequestBuilders
                 .get("/join").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    /**
+     * Get pyyntö polkuun "/pro" palauttaa merkkijonon.
+     * @throws Exception
+     */
+    @Test
+    public void getToProIsValid() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/pro").content("Tänne tulisi hoitajan näkymä"));
     }
 }
