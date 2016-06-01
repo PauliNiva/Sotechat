5e08b0d193569b399c5687c2cd6e2d11559c860e
PauliNiva <Pauli@Vulcan.local>
2016-06-01 14:20:25 +0300

    getters and setters

--- src/main/webapp/services/queue.service.js
+++ src/main/webapp/services/queue.service.js
@@ -5,7 +5,36 @@ angular.module('chatApp', [])
         var userID;
         var queue = {
             setChannelID : setChannelID,
-            
+            setUserName : setUserName,
+            setUserID : setUserID,
+            getChannelID : getChannelID,
+            getUserName : getUserName,
+            getUserID : getUserID
         };
 
+        function setChannelID(value) {
+            channelID = value;
+        };
+
+        function setUserName(value) {
+            userName = value;
+        };
+
+        function setUserID(value) {
+            userID = value;
+        };
+
+        function getChannelID() {
+            return channelID;
+        };
+
+        function getUserName() {
+            return userName;
+        };
+
+        function getUserID() {
+            return userID;
+        };
+
+        return queue;
     });
