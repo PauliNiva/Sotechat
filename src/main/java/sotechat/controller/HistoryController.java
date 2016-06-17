7915aadf9c9c518451b633a23d5929c08d6f8c75
Salla-Mari Varkoi <salla_mari@hotmail.com>
2016-06-17 23:01:20 +0300

    controlleria

--- src/main/java/sotechat/controller/HistoryController.java
+++ src/main/java/sotechat/controller/HistoryController.java
@@ -4,20 +4,20 @@ import org.apache.xpath.operations.String;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Asus on 17.6.2016.
  */
 
 @Controller
-@RequestMapping('/proHistory')
 public class HistoryController {
 
     public HistoryController(){
 
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/proHistory" method = RequestMethod.GET)
     public final String showHistory(){
         return "forward:/chatHistory.html";
     }
@@ -26,4 +26,8 @@ public class HistoryController {
     public final String showConversation(){
         return "forward:/conversation.html";
     }
+
+    @RequestMapping(value = "/messages/{channelId}" method = RequestMethod.GET)
+    @ResponseBody
+    public final List<>
 }
