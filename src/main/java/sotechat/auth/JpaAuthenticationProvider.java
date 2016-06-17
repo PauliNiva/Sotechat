7c31bd24e1fce4be8237a674d63831ae8f32ace2
Pauli Niva <Pauli@Vulcan.local>
2016-06-17 11:33:38 +0300

    Update docs and minor fixes

--- src/main/java/sotechat/auth/JpaAuthenticationProvider.java
+++ src/main/java/sotechat/auth/JpaAuthenticationProvider.java
@@ -2,15 +2,19 @@ package sotechat.auth;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication
+        .AuthenticationProvider;
+import org.springframework.security.authentication
+        .UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
+
 import sotechat.domain.Person;
 import sotechat.repo.PersonRepo;
 
@@ -21,26 +25,31 @@ public class JpaAuthenticationProvider implements AuthenticationProvider {
     private PersonRepo personRepo;
 
     @Override
-    public Authentication authenticate(Authentication a) throws AuthenticationException {
+    public Authentication authenticate(Authentication a)
+            throws AuthenticationException {
         String loginName = a.getPrincipal().toString();
         String password = a.getCredentials().toString();
 
         Person person = personRepo.findByLoginName(loginName);
 
         if (person == null) {
-            throw new AuthenticationException("Unable to authenticate user " + loginName) {
+            throw new AuthenticationException(
+                    "Unable to authenticate user " + loginName) {
             };
         }
 
-        if (!BCrypt.hashpw(password, person.getSalt()).equals(person.getPassword())) {
-            throw new AuthenticationException("Unable to authenticate user " + loginName) {
+        if (!BCrypt.hashpw(password, person.getSalt())
+                .equals(person.getPassword())) {
+            throw new AuthenticationException(
+                    "Unable to authenticate user " + loginName) {
             };
         }
 
         List<GrantedAuthority> grantedAuths = new ArrayList<>();
         grantedAuths.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
 
-        return new UsernamePasswordAuthenticationToken(person.getLoginName(), password, grantedAuths);
+        return new UsernamePasswordAuthenticationToken(person.getLoginName(),
+                password, grantedAuths);
     }
 
     @Override
