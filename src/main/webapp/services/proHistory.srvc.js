1ba9d3a866dafa4641278734455fe196040139f5
Salla-Mari Varkoi <salla_mari@hotmail.com>
2016-06-17 18:18:25 +0300

    kokeilua

---
+++ src/main/webapp/services/proHistory.srvc.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Asus on 17.6.2016.
+ */
+
+angular.module('chatProApp')
+    .factory('proHistoryService', ['http', function ($http) {
+        
+        var PROHISTORYURL = '/proHistory';
+        
+        
+    }]);
