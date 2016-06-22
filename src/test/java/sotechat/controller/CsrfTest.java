f4b12ecda51046b749e3f22c66bad8fb35231eec
Oodi <simo.auvinen@helsinki.fi>
2016-06-22 22:24:39 +0300

    CsrfTest

---
+++ src/test/java/sotechat/controller/CsrfTest.java
@@ -0,0 +1,70 @@
+package sotechat.controller;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import sotechat.Launcher;
+import javax.servlet.Filter;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Launcher.class)
+@WebAppConfiguration
+public class CsrfTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    private MockMvc mvc;
+
+
+    /**
+     * Before.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilters(springSecurityFilterChain)
+                .build();
+    }
+
+    @Test
+    public void testCsrfValid() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .post("/toServer/heartBeat").with(csrf())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.heartbeat", is("server-alive")));
+    }
+
+    @Test
+    public void testCsrfInValid() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .post("/toServer/heartBeat").with(csrf().asHeader().useInvalidToken())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+}
